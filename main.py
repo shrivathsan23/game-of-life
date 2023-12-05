cb93762b2f2766c6855db6126e60a2ab351ffa1e
Shrivathsan Sridhar <vathsansridhar@gmail.com>
2023-12-05 21:00:44 +0530

    Added Cell class

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import time
-from copy import deepcopy
 from threading import Thread, Event
 
 from tkinter import Tk, Button, Label, Frame
+
+class State:
+    DEAD_CELL = 'lightgray'
+    ALIVE_CELL = 'gray'
+
+class Cell(Label):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.config(bg = State.DEAD_CELL)
+        self.state = False
+    
+    def toggle_state(self):
+        self.config(bg = State.DEAD_CELL if self.cget('bg') == State.ALIVE_CELL else State.ALIVE_CELL)
+        self.state = not self.state
+    
+    def get_state(self):
+        return self.state
+    
+    def set_state(self, state):
+        if self.state != state:
+            self.toggle_state()
 
 class Board(Tk):
     def __init__(self, ROWS = 25, COLS = 25):
@@ -10,35 +31,24 @@ class Board(Tk):
 
         self.ROWS = ROWS
         self.COLS = COLS
-
-        self.DEAD_CELL = 'lightgray'
-        self.ALIVE_CELL = 'gray'
-
+        
         self.title('Game of Life')
         self.resizable(False, False)
-        
+
         self.cells_frame = Frame(self, bg = 'white', relief = 'sunken')
         self.cells_frame.pack()
 
-        self.cell_labels = []
-        self.cell_values = []
-
+        self.cells = [[None for j in range(self.COLS)] for i in range(self.ROWS)]
         self.stop_event = None
 
         for i in range(self.ROWS):
-            row_cells = []
-            row_values = []
-
             for j in range(self.COLS):
-                cell = Label(self.cells_frame, text = '', width = 2, relief = 'raised', bg = self.DEAD_CELL)
+                cell = Cell(self.cells_frame, text = '', width = 2, relief = 'raised')
+                
                 cell.grid(row = i, column = j)
-                cell.bind('<Button-1>', lambda event, cell = cell, i = i, j = j: self.toggle_label(event, cell, i, j))
-
-                row_cells.append(cell)
-                row_values.append(False)
-            
-            self.cell_labels.append(row_cells)
-            self.cell_values.append(row_values)
+                cell.bind('<Button-1>', lambda event, cell = cell: self.toggle_cell(cell))
+                
+                self.cells[i][j] = cell
         
         self.control_frame = Frame(self)
         self.control_frame.pack()
@@ -53,25 +63,13 @@ class Board(Tk):
         self.next_gen_btn.pack(side = 'right')
 
         self.update_idletasks()
-
-        self.root_width, self.root_height = tuple(map(int, self.geometry().split('+')[0].split('x')))
-
-        self.screen_width = self.winfo_screenwidth()
-        self.screen_height = self.winfo_screenheight()
-
-        self.x = ((self.screen_width // 2) - (self.root_width // 2))
-        self.y = ((self.screen_height // 2) - (self.root_height // 2))
-
-        self.geometry(f'{self.root_width}x{self.root_height}+{self.x}+{self.y}')
+        self.geometry(f'{self.winfo_reqwidth()}x{self.winfo_reqheight()}+10+10')
 
         self.mainloop()
     
-    def toggle_label(self, event, cell, i, j):
+    def toggle_cell(self, cell):
         if self.stop_event is None:
-            color = self.DEAD_CELL if cell.cget('bg') == self.ALIVE_CELL else self.ALIVE_CELL
-            cell.config(bg = color)
-
-            self.cell_values[i][j] = not self.cell_values[i][j]
+            cell.toggle_state()
     
     def is_safe(self, i, j):
         return 0 <= i < self.ROWS and 0 <= j < self.COLS
@@ -84,21 +82,21 @@ class Board(Tk):
                 if x == 0 and y == 0:
                     continue
 
-                I, J = i + x, j + y
+                new_i, new_j = i + x, j + y
 
-                if self.is_safe(I, J) and self.cell_values[I][J]:
+                if self.is_safe(new_i, new_j) and self.cells[new_i][new_j].get_state():
                     neighbor_count += 1
         
         return neighbor_count
     
     def calc_next_gen(self):
-        next_gen_values = deepcopy(self.cell_values)
+        next_gen_values = [[self.cells[i][j].get_state() for j in range(self.COLS)] for i in range(self.ROWS)]
 
         for i in range(self.ROWS):
             for j in range(self.COLS):
                 neighbor_count = self.count_neighbors(i, j)
 
-                if self.cell_values[i][j]:
+                if self.cells[i][j].get_state():
                     if neighbor_count < 2 or neighbor_count > 3:
                         next_gen_values[i][j] = False
                 
@@ -106,50 +104,24 @@ class Board(Tk):
                     if neighbor_count == 3:
                         next_gen_values[i][j] = True
         
-        self.cell_values = deepcopy(next_gen_values)
-
-        for i in range(self.ROWS):
-            for j in range(self.COLS):
-                color = self.ALIVE_CELL if self.cell_values[i][j] else self.DEAD_CELL
-                self.cell_labels[i][j].config(bg = color)
+        [[self.cells[i][j].set_state(next_gen_values[i][j]) for j in range(self.COLS)] for i in range(self.ROWS)]
     
     def start_gen(self):
         while self.stop_event is not None:
-            next_gen_values = deepcopy(self.cell_values)
-
-            for i in range(self.ROWS):
-                for j in range(self.COLS):
-                    neighbor_count = self.count_neighbors(i, j)
-
-                    if self.cell_values[i][j]:
-                        if neighbor_count < 2 or neighbor_count > 3:
-                            next_gen_values[i][j] = False
-                    
-                    else:
-                        if neighbor_count == 3:
-                            next_gen_values[i][j] = True
-            
-            self.cell_values = deepcopy(next_gen_values)
-
-            for i in range(self.ROWS):
-                for j in range(self.COLS):
-                    color = self.ALIVE_CELL if self.cell_values[i][j] else self.DEAD_CELL
-                    self.cell_labels[i][j].config(bg = color)
+            self.calc_next_gen()
+            time.sleep(0.025)
     
     def clear_cells(self):
-        for i in range(self.ROWS):
-            for j in range(self.COLS):
-                self.cell_labels[i][j].config(bg = self.DEAD_CELL)
-                self.cell_values[i][j] = False
+        [[self.cells[i][j].set_state(False) for j in range(self.COLS)] for i in range(self.ROWS)]
     
     def start_gen_thread(self):
         if self.stop_event is None:
             self.start_btn.config(text = 'Stop')
+
             self.clear_btn['state'] = 'disabled'
             self.next_gen_btn['state'] = 'disabled'
 
             self.stop_event = Event()
-
             Thread(target = self.start_gen).start()
         
         else:
@@ -157,8 +129,9 @@ class Board(Tk):
             self.stop_event = None
 
             self.start_btn.config(text = 'Start')
+
             self.clear_btn['state'] = 'normal'
             self.next_gen_btn['state'] = 'normal'
 
 if __name__ == '__main__':
-    Board()
+    Board(35, 76)
